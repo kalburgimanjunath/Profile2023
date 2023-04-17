b654f2af707e9c62689af391e315cf99457520be
manjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-17 19:20:03 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -601,7 +601,7 @@ const publicationsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "",
+    profile_image_path: "animated_ashutosh.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, Full-Stack, Android, Cloud and Opensource Development.",
   },
